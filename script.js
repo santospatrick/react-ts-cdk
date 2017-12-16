164b904e13feeffd9823c4af90d712226ded08f6
Patrick Santos <patrickspteixeira@gmail.com>
2017-12-16 00:49:50 -0200

    add simple ts types example

---
+++ script.js
@@ -0,0 +1,14 @@
+// string
+var myName = 'Max';
+myName = 28;
+// number
+var myAge = 10;
+myAge = 'nice error';
+// boolean
+var hasSomething = true;
+hasSomething = 1;
+// assign types
+var vrau;
+vrau = 27;
+vrau = '27';
+//# sourceMappingURL=script.js.map
