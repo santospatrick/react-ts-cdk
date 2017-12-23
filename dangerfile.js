ad03a781810f7fa908f0eea37051db44ca13f3fa
renato-bohler <renato.bohler@gmail.com>
2017-12-23 00:18:32 -0200

    fix(dangerfile): fixes message for big PRs

--- dangerfile.js
+++ dangerfile.js
@@ -12,11 +12,9 @@ if (packageChanged && !lockfileChanged) {
 // Warn when there is a big PR
 const bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(":exclamation: Big PR (" + ++errorCount + ")");
+  warn(":exclamation: Big PR");
   markdown(
-    "> (" +
-      errorCount +
-      ") : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review."
+    "> Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review."
   );
 }
 
