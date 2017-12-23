3f23d60cd334e844162962e9981ddcc33469bcb6
renato-bohler <renato.bohler@gmail.com>
2017-12-23 00:41:02 -0200

    refactor(dangerfile): adds packages without updating yarn.lock file to test dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -20,7 +20,7 @@ if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
 
 // Always ensure we assign someone, so that our Slackbot can do its work correctly
 if (danger.github.pr.assignee === null) {
-  fail(
+  warn(
     "Please assign someone to merge this PR, and optionally include people who should review."
   );
 }
