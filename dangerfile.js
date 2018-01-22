ed0ee97e96054e37d04d3a714d906b3357d2fd3b
renato-bohler <renato.bohler@gmail.com>
2018-01-22 21:45:46 -0200

    test(dangerjs): adds new checks for updated and removed dependencies
    #41

--- dangerfile.js
+++ dangerfile.js
@@ -49,3 +49,149 @@ schedule(async () => {
     }
   }
 });
+
+// Checks for removed dependencies
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+  if (packageDiff && packageDiff.dependencies) {
+    let dependenciesString;
+    const removedDependencies = packageDiff.dependencies.removed;
+
+    if (removedDependencies.length > 0) {
+      removedDependencies.forEach(newDependency => {
+        if (!dependenciesString) {
+          dependenciesString = newDependency;
+        } else {
+          dependenciesString = `${dependenciesString}, ${newDependency}`;
+        }
+      });
+      message(
+        `There is ${removedDependencies.length} removed ${
+          removedDependencies.length === 1 ? "dependency" : "dependencies"
+        } in this PR: ${dependenciesString}`
+      );
+    }
+  }
+});
+
+// Checks for updated dependencies
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+  if (packageDiff && packageDiff.dependencies) {
+    let updatedDependencies = [];
+    const dependencies = packageDiff.dependencies;
+
+    // For each dependency in the new package.json
+    Object.keys(dependencies.after).forEach(dependency => {
+      // If the dependency version has been updated
+      const versionBefore = dependencies.before[dependency];
+      const versionAfter = dependencies.after[dependency];
+      if (versionBefore && versionAfter !== versionBefore) {
+        updatedDependencies.push(
+          `${dependency} (from <b>${versionBefore}</b> to <b>${versionAfter}</b>)`
+        );
+      }
+    });
+
+    let updatedString;
+    if (updatedDependencies.length > 0) {
+      updatedDependencies.forEach(updatedDependency => {
+        if (!updatedString) {
+          updatedString = updatedDependency;
+        } else {
+          updatedString = `${updatedString}, ${updatedDependency}`;
+        }
+      });
+      message(`The version of ${updatedDependencies.length} 
+      ${
+        updatedDependencies.length === 1 ? "dependency" : "dependencies"
+      } have been updated in this PR: 
+      ${updatedString}`);
+    }
+  }
+});
+
+// Checks for new devDependencies
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+  if (packageDiff && packageDiff.devDependencies) {
+    let devDependenciesString;
+    const newDevDependencies = packageDiff.devDependencies.added;
+
+    if (newDevDependencies.length > 0) {
+      newDevDependencies.forEach(newDevDependency => {
+        if (!devDependenciesString) {
+          devDependenciesString = newDevDependency;
+        } else {
+          devDependenciesString = `${devDependenciesString}, ${newDevDependency}`;
+        }
+      });
+      message(
+        `There are ${newDevDependencies.length} new dev${
+          newDevDependencies.length === 1 ? "Dependency" : "Dependencies"
+        } added in this PR: ${devDependenciesString}`
+      );
+    }
+  }
+});
+
+// Checks for removed devDependencies
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+  if (packageDiff && packageDiff.devDependencies) {
+    let devDependenciesString;
+    const removedDevDependencies = packageDiff.devDependencies.removed;
+
+    if (removedDevDependencies.length > 0) {
+      removedDevDependencies.forEach(newDevDependency => {
+        if (!devDependenciesString) {
+          devDependenciesString = newDevDependency;
+        } else {
+          devDependenciesString = `${devDependenciesString}, ${newDevDependency}`;
+        }
+      });
+      message(
+        `There is ${removedDevDependencies.length} removed dev${
+          removedDevDependencies.length === 1 ? "Dependency" : "Dependencies"
+        } in this PR: ${devDependenciesString}`
+      );
+    }
+  }
+});
+
+// Checks for updated devDependencies
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+  if (packageDiff && packageDiff.devDependencies) {
+    let updatedDependencies = [];
+    const devDependencies = packageDiff.devDependencies;
+
+    // For each devDependency in the new package.json
+    Object.keys(devDependencies.after).forEach(devDependency => {
+      // If the devDependency version has been updated
+      const versionBefore = devDependencies.before[devDependency];
+      const versionAfter = devDependencies.after[devDependency];
+      if (versionBefore && versionAfter !== versionBefore) {
+        updatedDependencies.push(
+          `${devDependency} (from <b>${versionBefore}</b> to <b>${versionAfter}</b>)`
+        );
+      }
+    });
+
+    let updatedString;
+    if (updatedDependencies.length > 0) {
+      updatedDependencies.forEach(updatedDependency => {
+        if (!updatedString) {
+          updatedString = updatedDependency;
+        } else {
+          updatedString = `${updatedString}, ${updatedDependency}`;
+        }
+      });
+      message(`The version of ${updatedDependencies.length} dev
+      ${
+        updatedDependencies.length === 1 ? "Dependency" : "Dependencies"
+      } have been updated in this PR: 
+      ${updatedString}`);
+    }
+  }
+});
